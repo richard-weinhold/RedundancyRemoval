1feb88b157cbfe40717598c5fcf96d445956b3dc
riw <riw@wip.tu-berlin.de>
2021-02-03 12:53:20 +0100

    Add compat to requirements

--- src/RedundancyRemoval.jl
+++ src/RedundancyRemoval.jl
@@ -3,7 +3,7 @@ module RedundancyRemoval
 using CSV, DataFrames, Dates, Logging, LoggingExtras
 using LinearAlgebra
 using JuMP
-using Base.Threads, ThreadTools
+using Base.Threads
 
 include("redundancy_removal_functions.jl")
 include("main.jl")
